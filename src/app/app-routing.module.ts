d3223f5b3292b057130df80aa5bca57d10481a29
Omar <mromarhesham1999@gmail.com>
2023-03-30 17:09:04 +0200

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorsComponent } from './authors/authors.component';
+import { BooksComponent } from './books/books.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , redirectTo:'categories', pathMatch:'full'},
+  {path:'categories' , component:CategoriesComponent},
+  {path:'authors' , component:AuthorsComponent},
+  {path:'books' , component:BooksComponent},
+  {path:'login' , component:LoginComponent},
+  {path:'**' , component:NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { BooksComponent } from './books/books.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    CategoriesComponent,
+    BooksComponent,
+    AuthorsComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
